d1ee956ac40b29bf782eea357d404aaad502f3e5
yshashanky <812shanky@gmail.com>
2023-01-14 00:22:44 +0530

    code change

--- Ball.lua
+++ Ball.lua
@@ -27,6 +27,27 @@ function Ball:init(x, y, width, height)
 end
 
 --[[
+    Expects a paddle as an argument and returns true or false, depending
+    on whether their rectangles overlap.
+]]
+function Ball:collides(paddle)
+    -- first, check to see if the left edge of either is farther to the right
+    -- than the right edge of the other
+    if self.x > paddle.x + paddle.width or paddle.x > self.x + self.width then
+        return false
+    end
+
+    -- then check to see if the bottom edge of either is higher than the top
+    -- edge of the other
+    if self.y > paddle.y + paddle.height or paddle.y > self.y + self.height then
+        return false
+    end 
+
+    -- if the above aren't true, they're overlapping
+    return true
+end
+
+--[[
     Places the ball in the middle of the screen, with an initial random velocity
     on both axes.
 ]]
